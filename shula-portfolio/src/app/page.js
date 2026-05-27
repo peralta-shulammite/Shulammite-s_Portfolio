6b0a99382811a52f12d2afa9270fdc058b758186
peralta-shulammite <peraltashulammite@gmail.com>
2026-05-27 13:36:39 +0800

    initial commit

---
+++ shula-portfolio/src/app/page.js
@@ -0,0 +1,11 @@
+import Navbar from '@/components/navbar/page'
+import Hero from '@/sections/Hero'
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  )
+}
